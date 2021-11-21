4be69d5506d14eaae8e1835dc82f24d1cf73603d
ThaiNgocHuuBang <86879477+ThaiNgocHuuBang@users.noreply.github.com>
2021-11-21 19:05:00 +0700

    set up font text, Route,

---
+++ UEHVote/UEHVote/Common/Routes.cs
@@ -0,0 +1,13 @@
+﻿namespace VMS.Common
+{
+    public static class Routes
+    {
+        #region Main
+
+        public const string HomePage = "/";
+        public const string RankPage = "/bang-xep-hang";
+        public const string DetailVote = "/chi-tiet-de-cu";
+
+        #endregion Admin
+    }
+}
