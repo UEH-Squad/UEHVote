12df799f42cb13b3e6077ad40fd83756896b4832
lananhkn <anhkhuu.31191023029@st.ueh.edu.vn>
2021-12-04 11:05:56 +0700

    add pictureswall component

--- UEHVote/UEHVote/Pages/NominationEdit/Nomination.cs
+++ UEHVote/UEHVote/Pages/NominationEdit/Nomination.cs
@@ -15,12 +15,6 @@ namespace UEHVote.Pages.NominationEdit
         public string Content { get; set; }
 
         public string VideoLink { get; set; }
-
-        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
-        public string ElectionName { get; set; }
-
-        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
-        public string OrgCreatesElection { get; set; }
         public string Banner { get; set; }
 
     }
