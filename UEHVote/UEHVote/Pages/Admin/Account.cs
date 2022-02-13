8131b291330089e3e1e8515ba3b672b6a9ea31b5
lananhkn <anhkhuu.31191023029@st.ueh.edu.vn>
2022-02-13 16:56:51 +0700

    add student management list

---
+++ UEHVote/UEHVote/Pages/Admin/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UEHVote.Pages.Admin
+{
+    public class Account
+    {
+        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
+        public string Organization { get; set; }
+
+        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
+        public string StudentId { get; set; }
+
+        [Required(ErrorMessage = "Trường thông tin bắt buộc")]
+        public string Email { get; set; }
+
+    }
+}
